2ba0685b8ff76728fe85a19f7d5df1f28f6afae8
fz <phiane.d@gmail.com>
2022-12-30 11:06:50 +0100

    sal

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const toggleBtn= document.getElementsByClassName('burger')[0]
-const navBar=document.getElementsByClassName('nav_links')[0]
+import sal from "sal.js";
+const toggleBtn = document.getElementsByClassName("burger")[0];
+const navBar = document.getElementsByClassName("nav_links")[0];
 
-toggleBtn.addEventListener('click', ()=>{
-  navBar.classList.toggle('active')
-})
+toggleBtn.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+});
+sal();
