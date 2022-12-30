d7bed593eb675ea913c0bf7c51d836e3a1bcf813
fz <phiane.d@gmail.com>
2022-12-30 11:08:51 +0100

    sal

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-import sal from "sal.js";
 const toggleBtn = document.getElementsByClassName("burger")[0];
 const navBar = document.getElementsByClassName("nav_links")[0];
 
 toggleBtn.addEventListener("click", () => {
   navBar.classList.toggle("active");
 });
-sal();
